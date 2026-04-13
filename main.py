514af8cdf7a47aa4a6991c087ce97d08d0b888db
aminoka-play <aminokaanarbaeva@gmail.com>
2026-04-13 21:15:41 +0600

    add project files
    add project files

---
+++ main.py
@@ -0,0 +1,18 @@
+print("~~~ ЗАМЕНА СЛОВ В ТЕКСТЕ ~~~")
+
+text = input("Введите текст: ")
+
+if text.strip() == "":
+    print("Ошибка: текст пустой. Попробуйте снова.")
+else:
+    old_word = input("Какое слово заменить: ")
+    new_word = input("На что заменить: ")
+
+    if old_word in text:
+        result = text.replace(old_word, new_word)
+        print()
+        print("Готово! Вот результат:")
+        print(result)
+    else:
+        print()
+        print("Такого слова в тексте нет.")
