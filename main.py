07f562f5db7e112d5e110622b2b86709254925d6
aminoka-play <aminokaanarbaeva@gmail.com>
2026-04-13 21:12:44 +0600

    Implement input validation for empty text
    Added input validation for empty text before word replacement.

--- main.py
+++ main.py
@@ -2,14 +2,17 @@ print("~~~ ЗАМЕНА СЛОВ В ТЕКСТЕ ~~~")
 
 text = input("Введите текст: ")
 
-old_word = input("Какое слово заменить: ")
-new_word = input("На что заменить: ")
-
-if old_word in text:
-    result = text.replace(old_word, new_word)
-    print()
-    print("Готово!) Вот результат:")
-    print(result)
+if text.strip() == "":
+    print("Ошибка: текст пустой. Попробуйте снова.")
 else:
-    print()
-    print("Такого слова в тексте нет.")
+    old_word = input("Какое слово заменить: ")
+    new_word = input("На что заменить: ")
+
+    if old_word in text:
+        result = text.replace(old_word, new_word)
+        print()
+        print("Готово! Вот результат:")
+        print(result)
+    else:
+        print()
+        print("Такого слова в тексте нет.")
