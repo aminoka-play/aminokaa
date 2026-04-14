b343940c9bc68666ed82a25fcc1305a20591a6eb
aminoka-play <aminokaanarbaeva@gmail.com>
2026-04-14 18:22:19 +0600

    Enable case insensitive word replacement
    Enhance word replacement functionality to be case insensitive by comparing words in lowercase.

--- main.py
+++ main.py
@@ -8,8 +8,18 @@ else:
     old_word = input("Какое слово заменить: ")
     new_word = input("На что заменить: ")
 
-    if old_word in text:
-        result = text.replace(old_word, new_word)
+    if old_word.lower() in text.lower():
+        words = text.split()
+        result_words = []
+
+        for word in words:
+            if word.lower() == old_word.lower():
+                result_words.append(new_word)
+            else:
+                result_words.append(word)
+
+        result = " ".join(result_words)
+
         print()
         print("Готово! Вот результат:")
         print(result)
